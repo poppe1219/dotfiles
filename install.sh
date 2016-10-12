d9cad7d0db36552f1902c576eba6469fe6246c00
poppe1219 <robin.asen@gmail.com>
2016-10-12 21:40:06 +0200

    Adjustments to link .zshrc correctly, temporary disable of arc theme install.

--- install.sh
+++ install.sh
@@ -35,7 +35,6 @@ chown -R "$SUDO_USER:users" $HOME_PATH
 
 cd "$HOME_PATH"
 ln -s "$HOME_PATH/git/dotfiles/.gtkrc-2.0" .gtkrc-2.0
-ln -s "$HOME_PATH/git/dotfiles/.zshrc" .zshrc
 ln -s "$HOME_PATH/git/dotfiles/.Xresources" .Xresources
 cd "$HOME_PATH/.config/i3"
 ln -s "$HOME_PATH/git/dotfiles/.config/i3/config" config
@@ -55,15 +54,15 @@ if ! grep "\[archlinuxfr\]" /etc/pacman.conf ; then
 fi
 
 pacman -Sy --noconfirm yaourt
-pacman -S --noconfirm lxappearance feh sddm xterm rofi rxvt-unicode xorg-xrdb firefox tig
+pacman -S --noconfirm lxappearance feh sddm xterm rofi rxvt-unicode xorg-xrdb firefox tig vim tmux
 sudo systemctl enable sddm.service
 sudo -u $SUDO_USER yaourt -S --noconfirm i3-gaps-git i3lock ttf-iosevka zsh
+chsh -s /bin/zsh $SUDO_USER  # Set default shell to zsh.
 touch "$HOME_PATH/.xinitrc"
 echo "exec i3 > ~/.i3.log 2>&1" >> "$HOME_PATH/.xinitrc"
 
 chown -R $SUDO_USER:users "$HOME_PATH/.xinitrc"
-pacman -S --noconfirm vim tmux
-sudo -u $SUDO_USER yaourt -S --noconfirm gtk-theme-arc-grey-git
+#sudo -u $SUDO_USER yaourt -S --noconfirm gtk-theme-arc-grey-git
 
 cd Downloads
 wget -q https://github.com/supermarin/YosemiteSanFranciscoFont/archive/master.zip
@@ -75,8 +74,10 @@ rm -rf YosemiteSanFranciscoFont-master
 rm -f master.zip
 
 sudo -u $SUDO_USER sh -c "$(wget https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
-chsh -s /usr/bin/zsh $SUDO_USER
-cd .wallpapers
+rm -f ".zshrc"
+ln -s "$HOME_PATH/git/dotfiles/.zshrc" .zshrc
+
+cd "$HOME_PATH/.wallpapers"
 wget https://wallpaperscraft.com/image/frog_reflection_vector_24442_3840x2400.jpg
 cd $HOME_PATH
 sudo -u $SUDO_USER feh --bg-scale "$HOME_PATH/.wallpapers/frog_reflection_vector_24442_3840x2400.jpg"

86097ffa8029c42d48c3b32d1d8c8d79d7c819e7
poppe1219 <robin.asen@gmail.com>
2016-10-17 15:36:19 +0200

    Splitting in two terminals and performing two separate tasks.

--- install_after_x.sh
+++ install_after_x.sh
@@ -3,10 +3,7 @@
 xrandr --output `xrandr --listmonitors | grep "0:" | cut -d " " -f 6` --auto
 nitrogen --restore
 
-i3-msg split v
-i3-msg exec "i3-sensible-terminal"
-sleep 0.2
 i3-msg split h
-i3-msg exec "i3-sensible-terminal"
+i3-msg exec "i3-sensible-terminal -e top"
 
 htop

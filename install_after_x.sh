b102713c090c6907e5d27327cd84d950807792c8
poppe1219 <robin.asen@gmail.com>
2016-10-17 15:26:01 +0200

    Splitting terminals in secondary install script.

--- install_after_x.sh
+++ install_after_x.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
 
 xrandr --output `xrandr --listmonitors | grep "0:" | cut -d " " -f 6` --auto
+nitrogen --restore
+
+i3-msg split v
+i3-msg exec "i3-sensible-terminal"
+sleep 0.2
+i3-msg split h
+i3-msg exec "i3-sensible-terminal"
+
 htop

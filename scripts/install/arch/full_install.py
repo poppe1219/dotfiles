48ce60e7e6c8953b944c5ffbff947c6ed43e4092
poppe1219 <robin.asen@gmail.com>
2018-01-01 21:28:20 +0100

    Removed excess comment with bash code.

--- scripts/install/arch/full_install.py
+++ scripts/install/arch/full_install.py
@@ -32,21 +32,3 @@ if __name__ == "__main__":
     #var2 = run_script_2(var1)
     #var3 = run_script_3(var2)
     run_stuff()
-
-
-"""
-#!/bin/bash
-
-# Internet connectivity is assumed.
-
-sudo echo "Starting..."
-
-TARGET_DISK=${1}
-if [ -z "${TARGET_DISK}" ]; then
-    echo
-    echo "Target disk for installation. Typically 'sda'."
-    read -p "TARGET_DISK: " TARGET_DISK
-fi
-
-lsblk /dev/$TARGET_DISK
-"""
